35aee3a03bdf6a1a9f719bb9f6c09d42d698e625
ChristopherHerneth <christopher.herneth@gmail.com>
2025-07-11 18:23:56 +0200

    model and mass updates

--- muscle_model_run_v2.py
+++ muscle_model_run_v2.py
@@ -7,14 +7,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import socket
 import struct
+import threading
 
 udp_ip = '127.0.0.1'
 udp_recv = 12348
 udp_send = 12349
 BUFFER_SIZE = 8192
 
-recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-recv_socket.bind((udp_ip, udp_recv))
+# recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# recv_socket.bind((udp_ip, udp_recv))
+# recv_socket.setblocking(0)  # Set to non-blocking mode
 
 send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -118,10 +120,10 @@ class MuscleModel:
                 self.data.ctrl = ctrl
                 mujoco.mj_step(self.model, self.data)
             
-            elv_angle_torq = self.data.qfrc_actuator[self.elv_angle_id]
-            shoulder_elv_torq = self.data.qfrc_actuator[self.shoulder_elv_id]
-            shoulder_rot_torq = self.data.qfrc_actuator[self.rot_id]
-            elbow_torq = self.data.qfrc_actuator[self.elbow_id]
+            elv_angle_torq = self.data.qfrc_actuator[self.elv_angle_id] + self.data.qfrc_passive[self.elv_angle_id]
+            shoulder_elv_torq = self.data.qfrc_actuator[self.shoulder_elv_id] + self.data.qfrc_passive[self.shoulder_elv_id]
+            shoulder_rot_torq = self.data.qfrc_actuator[self.rot_id] + self.data.qfrc_passive[self.rot_id]
+            elbow_torq = self.data.qfrc_actuator[self.elbow_id] + self.data.qfrc_passive[self.elbow_id]
 
             torq = [elv_angle_torq[0], shoulder_elv_torq[0], shoulder_rot_torq[0], elbow_torq[0]]
             # print(activations)
@@ -146,7 +148,7 @@ class MuscleModel:
             options={'disp': False, 'maxiter': 100}
         )
         end_t = time.time_ns()
-        print(f"Optimization took {(end_t - start_t) / 1e6} ms")
+        # print(f"Optimization took {(end_t - start_t) / 1e6} ms")
 
         if not res.success:
             print("Optimization failed:", res.message)
@@ -162,7 +164,6 @@ class MuscleModel:
     
     def stiffness(self):
         K_muscle = []
-        # alpha = 23.4
         alpha = 23.4
         muscle_forces = []
 
@@ -232,7 +233,7 @@ class MuscleModel:
         pre_calc_state = np.empty(size, np.float64)
         mujoco.mj_getState(self.model, self.data, pre_calc_state, spec)
 
-        n_steps = 3
+        n_steps = 2
         act1 = 0.5
         act2 = 0.2
 
@@ -270,6 +271,10 @@ class MuscleModel:
         mujoco.mj_getState(self.model, self.data, state0, spec)
 
         mujoco.mj_step1(model.model, model.data)
+        # mujoco.mj_step(model.model, model.data)
+
+        passive_torques = np.array([self.data.qfrc_passive[id][0] for id in self.joint_id_list])
+        # print(f"Passive torques: {passive_torques}")
 
         jac = self.data.ten_J
         jac_1 = []
@@ -284,19 +289,22 @@ class MuscleModel:
         jac_1 = np.array(jac_1)
 
         M, c = self.linearize()
+        
+        A = jac_1.T @ M
+        C = jac_1.T @ c
 
         initial_guess = [self.act[i] for i in self.muscle_ids] #set initial guess to activations from previous step 
 
         mujoco.mj_setState(self.model, self.data, state0, spec)
 
         def cost_fn(activations):
-            return np.sum(activations**2)
-        
-        A = jac_1.T @ M
-        C = jac_1.T @ c
+            act_sum = 0
+            for a in activations:
+                act_sum += a**2
+            return act_sum
 
-        def constraint_fun(activations):
-            return A @ activations + C - req_torques
+        # def constraint_fun(activations):
+        #     return A @ activations + C - req_torques
         
         def constraint_jac(activations):
             return A
@@ -304,12 +312,16 @@ class MuscleModel:
         def objective_grad(activations):
             return 2 * activations
 
-        constraints = {
+        constraints = [{
             'type': 'eq',
-            'fun': constraint_fun,
-            # 'jac': constraint_jac,
+            'fun': lambda activations: A @ activations + C - req_torques,
+            'jac': lambda activations: A
             # 'tol': 1e-3
-        }
+        }]
+
+        # constraints = [
+        #     {'type': 'eq', 'fun': lambda activations: np.dot(A[i,:], activations) - req_torques[i] + C[i], 'jac': lambda activations: A[i,:]} for i in range(len(req_torques))
+        # ]
 
         bounds = [(0, 1)] * len(self.muscle_ids)
 
@@ -318,21 +330,39 @@ class MuscleModel:
             fun=cost_fn,
             x0=initial_guess,
             method='SLSQP',
-            constraints=[constraints],
+            constraints=constraints,
             bounds=bounds,
             options={'disp': False, 'maxiter': 100},
-            # jac= objective_grad
+            jac= lambda activations: 2 * activations
         )
         end_t = time.time_ns()
-        print(f"Optimization took {(end_t - start_t) / 1e6} ms")
+        # print(f"Optimization took {(end_t - start_t) / 1e6} ms")
 
         if not res.success:
             print("Optimization failed:", res.message)
+        
+        # for i, con in enumerate(constraints):
+        #     residual = con['fun'](res.x)
+        #     print(f"Constraint {i} ({con['type']}): residual = {residual + req_torques}")
 
-        elv_angle_torq = self.data.qfrc_actuator[self.elv_angle_id] 
-        shoulder_elv_torq = self.data.qfrc_actuator[self.shoulder_elv_id] 
-        shoulder_rot_torq = self.data.qfrc_actuator[self.rot_id] 
-        elbow_torq = self.data.qfrc_actuator[self.elbow_id]
+        activations = res.x
+        
+        mujoco.mj_setState(self.model, self.data, state0, spec)
+
+        ctrl = self.data.ctrl
+            
+        for idx, act in zip(self.muscle_ids, activations):
+            ctrl[idx] = act
+
+        self.data.ctrl = ctrl
+
+        for _ in range(5):
+            mujoco.mj_forward(self.model, self.data)
+
+        elv_angle_torq = self.data.qfrc_actuator[self.elv_angle_id] #+ self.data.qfrc_passive[self.elv_angle_id] + self.data.qfrc_applied[self.elv_angle_id]
+        shoulder_elv_torq = self.data.qfrc_actuator[self.shoulder_elv_id] #+ self.data.qfrc_passive[self.shoulder_elv_id] + self.data.qfrc_applied[self.shoulder_elv_id]
+        shoulder_rot_torq = self.data.qfrc_actuator[self.rot_id] #+ self.data.qfrc_passive[self.rot_id] + self.data.qfrc_applied[self.rot_id]
+        elbow_torq = self.data.qfrc_actuator[self.elbow_id] #+ self.data.qfrc_passive[self.elbow_id] + self.data.qfrc_applied[self.elbow_id]
 
         torq = [elv_angle_torq[0], shoulder_elv_torq[0], shoulder_rot_torq[0], elbow_torq[0]]
 
@@ -352,19 +382,91 @@ fail_count = 0
 time_count = 0
 error = 0
 
+class UDPReceiver:
+    def __init__(self, ip='localhost', port=12348, buffer_size=1024):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((ip, port))
+        self.sock.setblocking(False)  # Non-blocking socket
+        
+        self.buffer_size = buffer_size
+        self.latest_data = None
+        self.lock = threading.Lock()
+        self.running = False
+
+    def start(self):
+        self.running = True
+        self.thread = threading.Thread(target=self._receive_loop, daemon=True)
+        self.thread.start()
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
+
+    def _receive_loop(self):
+        while self.running:
+            try:
+                data, addr = self.sock.recvfrom(self.buffer_size)
+                with self.lock:
+                    self.latest_data = data
+            except BlockingIOError:
+                # No data available right now
+                pass
+
+    def get_latest_data(self):
+        with self.lock:
+            return self.latest_data
+
+
+# Initialize the UDP receiver
+udp_receiver = UDPReceiver(ip=udp_ip, port=udp_recv, buffer_size=BUFFER_SIZE)
+udp_receiver.start()
+
+spec = mujoco.mjtState.mjSTATE_INTEGRATION
+size = mujoco.mj_stateSize(model.model, spec)
+state0 = np.empty(size, np.float64)
+mujoco.mj_getState(model.model, model.data, state0, spec)
+count = 0
+print_iter = 0
+
+bodies = [mujoco.mj_id2name(model.model, mujoco.mjtObj.mjOBJ_BODY, i) for i in range(model.model.nbody)]
+forearm_mass = 1.1942
+hand_mass = 0.0182 + 0.7332
+
+ulna_id = mujoco.mj_name2id(model.model, mujoco.mjtObj.mjOBJ_BODY, "ulna")
+radius_id = mujoco.mj_name2id(model.model, mujoco.mjtObj.mjOBJ_BODY, "radius")
+hand_id_start = radius_id + 1
+model_fa_mass = model.model.body_mass[ulna_id] + model.model.body_mass[radius_id]
+model_hand_mass = sum(model.model.body_mass[i] for i in range(hand_id_start, model.model.nbody))
+fa_scale = forearm_mass / model_fa_mass
+hand_scale = hand_mass / model_hand_mass
+
+model.model.body_mass[ulna_id] *= fa_scale
+model.model.body_inertia[ulna_id] *= fa_scale
+model.model.body_mass[radius_id] *= fa_scale
+model.model.body_inertia[radius_id] *= fa_scale
+for i in range(hand_id_start, model.model.nbody):
+    model.model.body_mass[i] *= hand_scale
+    model.model.body_inertia[i] *= hand_scale
 
 ############################################################### MAIN LOOP ####################################################################
 while True:
-    start = time.time_ns()
-    data, addr = recv_socket.recvfrom(BUFFER_SIZE)
-    if len(data) == 22 * 8:
+    count += 1
+    if count == 1:
+        print_iter = 1
+        count = 0
+    else:
+        print_iter = 0
+    mujoco.mj_setState(model.model, model.data, state0, spec)
+    # data, addr = recv_socket.recvfrom(BUFFER_SIZE)
+    data = udp_receiver.get_latest_data()
+    if data is not None and len(data) == 22 * 8:
         values = struct.unpack('22d', data)
         torq = []
         shoulder_emg = values[0:4]
         arm_emg = values[4:8] #order: black, red, green, blue
         q_elbow = values[8] + np.pi/2
         q_shoulder = values[20:23]
-        q_shoulder = [0.1,0.1,0.1]
+        q_shoulder = [0.1,1.57,0.1]
         vel_elbow = values[12]
         vel_shoulder = values[23:26]
         vel_shoulder = [0,0,0]
@@ -376,21 +478,22 @@ while True:
             model.data.qvel[model.model.joint(model.joint_names[i]).qposadr] = qvel[i]
             # torq.append(values[8+i])
     else:
-        print("Received packet of invalid size:", len(data))
+        print("Received packet of invalid size")
         continue
-    torq = [0,0,0,torq_el]
+
+    torq = [0.001,0.001,0.001,torq_el]
     torq = np.array(torq)
     mujoco.mj_forward(model.model, model.data) #joint positions set above, so run fwd to set sim state
-    end_t = time.time_ns()
-    print(f"Received data in {(end_t - start) / 1e6} ms")
 
     # spec = mujoco.mjtState.mjSTATE_INTEGRATION
     # size = mujoco.mj_stateSize(model.model, spec)
-    # state0 = np.empty(size, np.float64)
-    # mujoco.mj_getState(model.model, model.data, state0, spec)
+    state1 = np.empty(size, np.float64)
+    mujoco.mj_getState(model.model, model.data, state1, spec)
     
-    activations, err, success, t, torq = model.optimize_fast(req_torques=torq)
-
+    start = time.time_ns()
+    activations, err, success, t, torq_out = model.optimize(req_torques=torq)
+    end_t = time.time_ns()
+    
     if not(success):
         fail_count += 1
     if fail_count > 0:
@@ -407,13 +510,13 @@ while True:
     else:
         error = 0
 
-    # mujoco.mj_setState(model.model, model.data, model.state0, model.spec)
+    mujoco.mj_setState(model.model, model.data, state1, spec)
     ctrl = model.data.ctrl
     
     for idx, act in zip(model.muscle_ids, activations):
         ctrl[idx] = act
 
-    blend = 0.1
+    blend = 1.0
     bicep_emg = (arm_emg[0] - 200)/4000
     tricep_emg = (arm_emg[1] - 200)/4000
 
@@ -426,25 +529,36 @@ while True:
     if bicep_emg > 1:
         bicep_emg = 1
 
+    if print_iter:
+        print(f"Received torques: {torq}")
+        print(f"Full Optimization took {(end_t - start) / 1e6} ms")
+        print(f"Torques: {torq_out}")
+
     # print("Bicep activation: ", bicep_emg)
     # print("Tricep activation: ", tricep_emg)
     for name in ["BIClong", "BICshort", "BRA", "BRD"]:
         a = ctrl[mujoco.mj_name2id(model.model, mujoco.mjtObj.mjOBJ_ACTUATOR, name)]
+        if print_iter:
+            print(f"{name} activation: {a}")
         ctrl[mujoco.mj_name2id(model.model, mujoco.mjtObj.mjOBJ_ACTUATOR, name)] = blend*a + (1-blend)*bicep_emg
     
     for name in ["TRIlong","TRIlat", "TRImed"]:
         a = ctrl[mujoco.mj_name2id(model.model, mujoco.mjtObj.mjOBJ_ACTUATOR, name)]
+        if print_iter:
+            print(f"{name} activation: {a}")
         ctrl[mujoco.mj_name2id(model.model, mujoco.mjtObj.mjOBJ_ACTUATOR, name)] = blend*a + (1-blend)*tricep_emg
     
+    model.data.ctrl = ctrl
     for _ in range(2):
-        model.data.ctrl = ctrl
         mujoco.mj_step(model.model, model.data)
+
     elbow_pos = model.data.qpos[model.elbow_id]
     elbow_vel = model.data.qvel[model.elbow_id]
-    # print(f"Elbow position: {elbow_pos}")
-    # print(f"Elbow velocity: {model.data.qvel[model.elbow_id]}")
-    # print(f"Elbow torque: {model.data.qfrc_actuator[model.elbow_id] + model.data.qfrc_passive[model.elbow_id] + model.data.qfrc_applied[model.elbow_id]}")
-    
+    if print_iter:
+        print(f"Elbow position: {elbow_pos}")
+        print(f"Elbow velocity: {model.data.qvel[model.elbow_id]}")
+        print(f"Elbow torque: {model.data.qfrc_actuator[model.elbow_id]} + {model.data.qfrc_passive[model.elbow_id]} = {model.data.qfrc_actuator[model.elbow_id] + model.data.qfrc_passive[model.elbow_id]}")
+        
     k = np.abs(model.stiffness())
 
     elbow_pos = float(elbow_pos)
--- udp_test.py
+++ udp_test.py
@@ -1,9 +1,14 @@
-import plotly.graph_objects as go
-import plotly.io as pio
+import socket
+import struct
 
-pio.renderers.default = 'browser'
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('localhost', 12348))
+sock.setblocking(False)  # or sock.settimeout(0.1)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=[0,1], y=[0,1], mode='lines'))
-
-fig.show()
+while True:
+    try:
+        data, addr = sock.recvfrom(1024)
+        values = struct.unpack('22d', data)
+        print(values)
+    except BlockingIOError:
+        pass  # no data yet, continue
